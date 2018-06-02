392ef82b561580c42885bd8b230ca931be50d6aa
hopshackle <james@janigo.co.uk>
2018-06-02 11:24:58 +0100

    Fixed bug in MCTS with rollout policy
    This now puts the acting players hand into the deck before makign a decision, and then removes it once decision is made, and before the action is applied to the gamestate.
    Otherwise we have a problem with the player never actually thinking they have the actual cards that they do have!

--- src/main/java/com/fossgalaxy/games/fireworks/ai/hopshackle/MCTSInfoSetPolicy.java
+++ src/main/java/com/fossgalaxy/games/fireworks/ai/hopshackle/MCTSInfoSetPolicy.java
@@ -2,7 +2,7 @@ package com.fossgalaxy.games.fireworks.ai.hopshackle;
 
 import com.fossgalaxy.games.fireworks.ai.Agent;
 import com.fossgalaxy.games.fireworks.annotations.AgentConstructor;
-import com.fossgalaxy.games.fireworks.state.GameState;
+import com.fossgalaxy.games.fireworks.state.*;
 import com.fossgalaxy.games.fireworks.state.actions.Action;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -34,7 +34,26 @@ public class MCTSInfoSetPolicy extends MCTSInfoSet {
     @Override
     protected Action selectActionForRollout(GameState state, int playerID) {
         try {
-            return rolloutPolicy.doMove(playerID, state);
+            // we first need to ensure Player's hand is back in deck
+            Hand myHand = state.getHand(playerID);
+            Deck deck = state.getDeck();
+            int cardsAddedToDeck = 0;
+            for (int i = 0; i < myHand.getSize(); i++) {
+                if (myHand.getCard(i) != null) {
+                    deck.add(myHand.getCard(i));
+                    //                System.out.println("Added " + myHand.getCard(i));
+                    cardsAddedToDeck++;
+                }
+            }
+            // then choose the action
+            Action chosenAction = rolloutPolicy.doMove(playerID, state);
+            // then put their hand back
+            for (int i = 0; i < cardsAddedToDeck; i++) {
+                Card removedCard = deck.getTopCard();
+                //         System.out.println("Removed " + removedCard);
+            }
+
+            return chosenAction;
         } catch (IllegalArgumentException ex) {
             LOG.error("warning, agent failed to make move: {}", ex);
             return super.selectActionForRollout(state, playerID);
